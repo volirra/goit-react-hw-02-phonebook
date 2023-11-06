96952c6622489ce60136b32dcedb0402e67b3a90
volirra <127318805+volirra@users.noreply.github.com>
2023-11-06 19:58:21 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,70 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import {}
-import { GlobalStyle } from './GlobalStyle.js';
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import initialContacts from './contacts.json';
+import { ContactList } from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 export class App extends Component {
-  return (
-    
-  );
-};
+  state = {
+    contacts: initialContacts,
+    filter: '',
+  };
+
+  addContact = newContact => {
+    this.state.contacts.filter(
+      contact =>
+        contact.name.toLowerCase().trim() ===
+          newContact.name.toLowerCase().trim() ||
+        contact.number.trim() === newContact.number.trim()
+    ).length
+      ? toast.error(`${newContact.name}: is already in contacts`, notifyOptions)
+      : this.setState(prevState => {
+          return {
+            contacts: [newContact, ...prevState.contacts],
+          };
+        });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value.toLowerCase() });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <Layout>
+        <Section title="Phonebook">
+          <ContactForm onAddContact={this.addContact} />
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            onDelete={this.deleteContact}
+          />
+        </Section>
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
